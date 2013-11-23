c8087f93f5bee7a9846079ebb313b035219080c8
alexander.vorkov <alexander.vorkov@teamdev.com>
2013-11-23 23:42:40 +0200

    deleted old files

--- ConsoleClient/src/com/teamdev/consoleclient/BinaryOperator.java
+++
@@ -1,9 +0,0 @@
-package com.teamdev.consoleclient;
-
-import java.math.BigDecimal;
-
-public interface BinaryOperator extends Comparable<BinaryOperator>{
-    public BigDecimal evaluate(BigDecimal first, BigDecimal second);
-    public int getPriority();
-    public String getName();
-}
